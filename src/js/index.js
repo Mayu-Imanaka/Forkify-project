27056bfcfceecd0056352cdd04379dcfbb746989
Mayu Imanaka <imamayu.1215@gmail.com>
2020-02-22 10:01:50 -0800

    add:get data from api on search page

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,31 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-async function getResults(query) {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2) New search object and add to start
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5)Render results on UI
+        searchView.renderResults(state.search.result);
+    }
 }
-getResults('pizza');
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
---
+++ src/js/views/base.js
@@ -0,0 +1,12 @@
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list')
+};
+
+export const renderLoader = parent => {
+    const loader = `
+    <div class="loader">
+    </div>
+    `
+};
